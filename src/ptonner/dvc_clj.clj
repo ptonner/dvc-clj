9cdf9b6d4794fb4c5035943d4d86290c3ff4beea
Peter Tonner <peter.tonner@gmail.com>
2023-05-07 14:56:59 -0400

    intermediate attempt at managing the YAML directly

--- src/ptonner/dvc_clj.clj
+++ src/ptonner/dvc_clj.clj
@@ -1,5 +1,64 @@
 (ns ptonner.dvc-clj
-  (:require [babashka.process :refer [shell process]]))
+  (:require [babashka.process :refer [shell process]]
+            [clj-yaml.core :as yaml]
+            [clojure.string :as str]
+            [babashka.fs :as fs]))
+
+(defn sym->str
+  [sym]
+  "Convert symbol to a string that can be resolved"
+  (-> sym
+      resolve
+      symbol
+      str))
+
+(defn resolve-string
+  [s]
+  "Resolve a string representing a symbol"
+  (-> s
+      symbol
+      resolve))
+
+(comment yaml/parse-stream
+         (-> "/tmp/dvc-test/abc.txt.dvc"
+             slurp
+             yaml/parse-string
+             (assoc :meta {::read (str (symbol (resolve 'slurp)))})
+             yaml/generate-string
+             yaml/parse-string
+             (get-in [:meta ::read])
+             symbol
+             resolve)
+         (require '[clojure.string :as str])
+         ((-> {:a (symbol (resolve 'str/split))}
+              str
+              clojure.edn/read-string
+              :a
+              resolve)
+          "a b c"
+          #" ")
+         (clojure.edn/read-string (str))
+         (-> (resolve 'yaml/parse-stream)
+             symbol
+             ;; (.. -ns)
+         ))
+
+;; (defn- to-meta-option [key val]
+;;   (str "--meta " (str key) "=" val))
+
+(defn- add-dvc-meta
+  [f m]
+  (spit f
+        (-> f
+            slurp
+            yaml/parse-string
+            (update :meta merge m)
+            ;; NOTE: use block style to remain similar to original
+            ;; style
+            (yaml/generate-string :dumper-options {:flow-style :block}))))
+
+(comment (add-dvc-meta "/tmp/dvc-test/abc.txt.dvc"
+                       {::read (sym->str 'slurp), ::write (sym->str 'spit)}))
 
 ;; Use cases:
 ;; - add an existing file (optionally load data?)
@@ -13,4 +72,11 @@
   [{:keys [target targets opt]}]
   (let [targets (or targets [target])
         opt (or opt {})]
-    (apply shell opt "dvc" "add" targets)))
+    ;; TODO: set default seder operation
+    ;; TODO: load target into memory and return it
+    (apply shell opt "dvc" "add" targets)
+    ;; Add metadata
+    ;; NOTE: this is broken b/c it doesn't track :dir option
+    (doseq [t targets]
+      (add-dvc-meta (str t ".dvc")
+                    {::read (sym->str 'slurp), ::write (sym->str 'spit)}))))
--- test/ptonner/dvc_clj_test.clj
+++ test/ptonner/dvc_clj_test.clj
@@ -6,8 +6,11 @@
             [babashka.process :refer [shell process]]
             [ptonner.dvc-clj :refer :all]))
 
-(defn- init-project
+(defn- init-project!
   [dir & files]
+  ;; start from empty directoy
+  (fs/delete-tree dir)
+  (fs/create-dirs dir)
   (shell {:dir (str dir)} "git init")
   (shell {:dir (str dir)} "dvc init")
   (doseq [f files] (fs/copy f dir)))
@@ -16,6 +19,13 @@
   (testing
    "DVC files added"
    (fs/with-temp-dir [tmp-dir nil]
-                     (init-project tmp-dir (io/resource "abc.txt"))
-                     (add {:opt {:dir (str tmp-dir)}, :target "abc.txt"})
+                     (init-project! tmp-dir (io/resource "abc.txt"))
+                     (add {:opt {:dir (str tmp-dir), :out :string, :err :out},
+                           :target "abc.txt"})
                      (is (fs/exists? (fs/path tmp-dir "abc.txt.dvc"))))))
+
+(comment (def tdir "/tmp/dvc-testing")
+         (init-project! tdir (io/resource "abc.txt"))
+         (-> (add {:opt {:dir tdir, :out :string, :err :string, :continue true},
+                   :target "abc.txt"})
+             :err))
