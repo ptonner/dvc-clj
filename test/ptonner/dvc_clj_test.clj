1011c6580ff2e477e48d73b58100aac5fedceccd
Peter Tonner <peter.tonner@gmail.com>
2023-05-08 21:12:38 -0400

    try to fix tests

--- test/ptonner/dvc_clj_test.clj
+++ test/ptonner/dvc_clj_test.clj
@@ -6,24 +6,26 @@
             [babashka.process :refer [shell process]]
             [ptonner.dvc-clj :refer :all]))
 
-(def dvc-clj-test-template-dir (fs/path "/" "tmp" "dvc-clj-test-template"))
+(def tmp-dir (fs/path "/" "tmp" "dvc-clj"))
+(fs/create-dirs tmp-dir)
+(def template-dir (fs/absolutize (fs/create-temp-dir {:path tmp-dir})))
 
 (defn- init-template!
   "Create a initial project to use as a template for tests"
   [t]
-  (fs/delete-tree dvc-clj-test-template-dir)
-  (fs/create-dirs dvc-clj-test-template-dir)
-  (let [cfg {:dir (str dvc-clj-test-template-dir), :out :string, :err :string}]
+  (fs/delete-tree template-dir)
+  (fs/create-dirs template-dir)
+  (let [cfg {:dir (str template-dir), :out :string, :err :string}]
     (shell cfg "git init")
     (shell cfg "dvc init"))
   (t)
-  (fs/delete-tree dvc-clj-test-template-dir))
+  (fs/delete-tree tmp-dir))
 
 (defn- copy-template!
   "Copy template to `dir`"
   [dir]
   (fs/delete-tree dir)
-  (fs/copy-tree dvc-clj-test-template-dir dir))
+  (fs/copy-tree template-dir dir))
 
 (use-fixtures :once init-template!)
 
@@ -34,15 +36,15 @@
   (doseq [f files] (fs/copy f dir)))
 
 (deftest find-root
-  (testing "root found"
-           (is (= (project-root dvc-clj-test-template-dir)
-                  dvc-clj-test-template-dir)))
-  (testing "no root" (is (fs/with-temp-dir [td {}] (nil? (project-root td))))))
+  (testing "root found" (is (= (project-root template-dir) template-dir)))
+  (testing "no root"
+           (is (fs/with-temp-dir [td {:path tmp-dir}]
+                                 (nil? (project-root td))))))
 
 (deftest add-file
   (testing
    "DVC files added"
-   (fs/with-temp-dir [tmp-dir nil]
+   (fs/with-temp-dir [tmp-dir {:path tmp-dir}]
                      (init-project! tmp-dir (io/resource "abc.txt"))
                      (add! {:opt {:dir (str tmp-dir), :out :string, :err :out},
                             :target "abc.txt"})
